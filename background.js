eb114a5e998b8dc1ae836fa601f0f0f9fede5a22
treeleaves30760 <treeleaves30760@gmail.com>
2023-03-08 14:02:06 +0800

    Feat: Move to V3

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onInstalled.addListener(function () {
-	console.log("MyWebSaver installed.");
-});
-
-chrome.browserAction.onClicked.addListener(function (tab) {
-	console.log("Save Web Page button clicked.");
-});
--- popup.js
+++ popup.js
@@ -1,92 +1,49 @@
-document.addEventListener("DOMContentLoaded", function () {
-	var saveButton = document.getElementById("saveTxt");
-	saveButton.addEventListener("click", function () {
-		chrome.tabs.query(
-			{ active: true, currentWindow: true },
-			function (tabs) {
-				var tab = tabs[0];
-				var title = tab.title;
-				var url = tab.url;
-				chrome.tabs.executeScript(
-					tab.id,
-					{
-						code: `
-                            var itemsStartElements = document.querySelectorAll('div.items-start'); 
-                            var textArray = Array.prototype.map.call(itemsStartElements, function(element) { return element.textContent; })
-                            textArray.pop()
-                            console.log(textArray);
-                            var textContent = "";
-                            for (var i = 0; i < textArray.length; i++) {
-                                if (i % 2 == 0) {
-                                    textContent += "User: \\n";
-                                } else {
-                                    textContent += "Chat: \\n";
-                                }
-                                textContent += textArray[i];
-                                textContent += "\\n";
-                            }
-                            textContent
-                        `,
-					},
-					function (results) {
-						var textContent = results[0];
-						chrome.downloads.download({
-							url:
-								"data:text/plain," +
-								encodeURIComponent(textContent),
-							filename: title + ".txt",
-							saveAs: true,
-						});
-					}
-				);
-			}
-		);
-	});
-});
+let count = 0;
+let error = "";
+function countAdd() {
+	count += 1;
+	document.getElementById("count").innerText = count;
+}
 
-document.addEventListener("DOMContentLoaded", function () {
-	var saveButton = document.getElementById("saveDocx");
-	saveButton.addEventListener("click", function () {
-		chrome.tabs.query(
-			{ active: true, currentWindow: true },
-			function (tabs) {
-				var tab = tabs[0];
-				var title = tab.title;
-				var url = tab.url;
-				chrome.tabs.executeScript(
-					tab.id,
-					{
-						code: `
-                            var itemsStartElements = document.querySelectorAll('div.items-start'); 
-                            var textArray = Array.prototype.map.call(itemsStartElements, function(element) { return element.textContent; })
-                            textArray.pop()
-                            console.log(textArray);
-                            var textContent = "";
-                            for (var i = 0; i < textArray.length; i++) {
-                                if (i % 2 == 0) {
-                                    textContent += "User: \\n";
-                                } else {
-                                    textContent += "Chat: \\n";
-                                }
-                                textContent += textArray[i];
-                                textContent += "\\n";
-                            }
-                            textContent
-                        `,
-					},
-					function (results) {
-						var textContent = results[0];
-						var blob = new Blob([textContent], {
-							type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-						});
-						chrome.downloads.download({
-							url: URL.createObjectURL(blob),
-							filename: title + ".docx",
-							saveAs: true,
-						});
-					}
-				);
-			}
-		);
+function updateError(error_msg) {
+	document.getElementById("Error_msg").innerHTML = error_msg;
+}
+
+function saveItems() {
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		chrome.scripting.executeScript({
+			target: { tabId: tabs[0].id },
+			func: saveItemsOnPage,
+		});
 	});
+}
+
+function saveItemsOnPage() {
+	var Elements = document.querySelectorAll("div.items-start");
+	var textArray = Array.prototype.map.call(Elements, function (element) {
+		return element.textContent;
+	});
+	textArray.pop();
+	var textContent = "";
+	for (var i = 0; i < textArray.length; i++) {
+		if (i % 2 == 0) {
+			textContent += "User: \n";
+		} else {
+			textContent += "Chat: \n";
+		}
+		textContent += textArray[i];
+		textContent += "\n";
+	}
+	console.log(textContent);
+	const filename = `items-${Date.now()}.txt`;
+	const blob = new Blob([textContent], { type: "text/plain" });
+	const url = URL.createObjectURL(blob);
+	var link = document.createElement("a");
+	document.body.appendChild(link);
+	link.href = url;
+	link.download = filename;
+	link.click();
+}
+document.addEventListener("DOMContentLoaded", function () {
+	document.getElementById("save-btn").addEventListener("click", saveItems);
 });

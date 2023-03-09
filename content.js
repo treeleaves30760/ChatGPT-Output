4057451d9d663177069f86e8862de0fe873b663d
treeleaves30760 <treeleaves30760@gmail.com>
2023-03-10 02:30:09 +0800

    Feat: Update permission and inject docx.js

--- content.js
+++ content.js
@@ -1,1 +1,4 @@
-
+const script = document.createElement("script");
+script.className = "AddByOut-ChatGPT";
+script.src = chrome.runtime.getURL("js/docx.js");
+document.head.append(script);
--- popup.js
+++ popup.js
@@ -1,14 +1,3 @@
-let count = 0;
-let error = "";
-function countAdd() {
-	count += 1;
-	document.getElementById("count").innerText = count;
-}
-
-function updateError(error_msg) {
-	document.getElementById("Error_msg").innerHTML = error_msg;
-}
-
 function saveItems() {
 	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 		chrome.scripting.executeScript({
@@ -42,7 +31,42 @@ function saveItemsOnPage() {
 	link.href = url;
 	link.download = filename;
 	link.click();
+	console.log(textContent);
 }
+
 document.addEventListener("DOMContentLoaded", function () {
 	document.getElementById("saveTXT").addEventListener("click", saveItems);
+});
+
+function saveItemsToDocx() {
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		chrome.scripting.executeScript({
+			target: { tabId: tabs[0].id },
+			func: saveItemsOnPageToDocx,
+		});
+	});
+}
+function saveItemsOnPageToDocx() {
+	var Elements = document.querySelectorAll("div.items-start");
+	var textArray = Array.prototype.map.call(Elements, function (element) {
+		return element.textContent;
+	});
+	textArray.pop();
+	var textContent = "";
+	for (var i = 0; i < textArray.length; i++) {
+		if (i % 2 == 0) {
+			textContent += "User: \n";
+		} else {
+			textContent += "Chat: \n";
+		}
+		textContent += textArray[i];
+		textContent += "\n";
+	}
+	alert("尚未完成");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	document
+		.getElementById("saveDocx")
+		.addEventListener("click", saveItemsToDocx);
 });
